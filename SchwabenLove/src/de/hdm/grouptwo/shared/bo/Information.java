2c00ec680a02eb47f966e397991994d81b5f6add
ManuelRuss <mr134@hdm-stuttgart.de>
2016-05-02 17:12:17 +0200

    Create Block, Visit, Information.

---
+++ SchwabenLove/src/de/hdm/grouptwo/shared/bo/Information.java
@@ -0,0 +1,15 @@
+package de.hdm.grouptwo.shared.bo;
+
+public class Information extends BusinessObject {
+
+	private String inputText;
+
+	public String getInputText() {
+		return inputText;
+	}
+
+	public void setInputText(String inputText) {
+		this.inputText = inputText;
+	}
+
+}
