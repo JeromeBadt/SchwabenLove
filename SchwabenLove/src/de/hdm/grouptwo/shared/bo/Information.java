87d900c9029548a18ed7fa2c9eccf9c966ff24f2
ManuelRuss <mr134@hdm-stuttgart.de>
2016-05-02 18:26:50 +0200

    Add Profile attributes.

--- SchwabenLove/src/de/hdm/grouptwo/shared/bo/Information.java
+++ SchwabenLove/src/de/hdm/grouptwo/shared/bo/Information.java
@@ -1,10 +1,18 @@
 package de.hdm.grouptwo.shared.bo;
-
+/** 
+ * 
+ * @author manuelruss
+ *
+ */
 public class Information extends BusinessObject {
 
 	
 	private static final long serialVersionUID = 1L;
 	private String inputText;
+	private Profile owner;
+	private Property property;
+	
+	
 
 	public String getInputText() {
 		return inputText;
@@ -12,6 +20,22 @@ public class Information extends BusinessObject {
 
 	public void setInputText(String inputText) {
 		this.inputText = inputText;
+	}
+
+	public Profile getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Profile owner) {
+		this.owner = owner;
+	}
+
+	public Property getProperty() {
+		return property;
+	}
+
+	public void setProperty(Property property) {
+		this.property = property;
 	}
 
 }
