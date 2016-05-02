9d48001a4e7b426dd09f3d3371922ec5cae9f900
chillingwithasmile <chillingwithasmile@10.60.44.68>
2016-05-02 16:45:47 +0200

    CBConnection & BO

--- SchwabenLove/src/de/hdm/grouptwo/shared/bo/BusinessObject.java
+++ SchwabenLove/src/de/hdm/grouptwo/shared/bo/BusinessObject.java
@@ -1,5 +1,55 @@
 package de.hdm.grouptwo.shared.bo;
 
-public class BusinessObject {
+import java.io.Serializable;
+
+/**
+ * 
+ * @author thies
+ *
+ */
+
+public abstract class BusinessObject implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private int id = 0;
+
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+
+  @Override
+public String toString() {
+    return this.getClass().getName() + " #" + this.id;
+  }
+
+  @Override
+public boolean equals(Object o) {
+
+    if (o != null && o instanceof BusinessObject) {
+      BusinessObject bo = (BusinessObject) o;
+      try {
+        if (bo.getId() == this.id)
+          return true;
+      }
+      catch (IllegalArgumentException e) {
+
+        return false;
+      }
+    }
+
+    return false;
+  }
+  
+  @Override
+public int hashCode() {
+	  return this.id;
+  }
 
 }
