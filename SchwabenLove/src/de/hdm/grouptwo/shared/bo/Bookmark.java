1affc8a0d821854c94a0dd69b991f020787d6fdc
JoshuaHill <jh147@hdm-stuttgart.de>
2016-05-02 17:55:54 +0200

    Add profile variables to classes

--- SchwabenLove/src/de/hdm/grouptwo/shared/bo/Bookmark.java
+++ SchwabenLove/src/de/hdm/grouptwo/shared/bo/Bookmark.java
@@ -1,10 +1,17 @@
 package de.hdm.grouptwo.shared.bo;
 
+/**
+ * A bookmark can be used by user to mark another profile for later use
+ * 
+ * @author joshuahill
+ *
+ */
 public class Bookmark extends BusinessObject {
 
 	private static final long serialVersionUID = 1L;
 
 	private BookmarkList bookmarkList;
+	private Profile profile;
 
 	public BookmarkList getBookmarkList() {
 		return bookmarkList;
@@ -12,5 +19,13 @@ public class Bookmark extends BusinessObject {
 
 	public void setBookmarkList(BookmarkList bookmarkList) {
 		this.bookmarkList = bookmarkList;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
 	}
 }
