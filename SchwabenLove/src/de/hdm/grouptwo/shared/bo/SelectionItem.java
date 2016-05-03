6328bae3c638db50a687d5af945a2a7c018db47b
DenisThierry <Dt019@hdm-stuttgart.de>
2016-05-03 19:22:13 +0200

    Insert mapper class

--- SchwabenLove/src/de/hdm/grouptwo/shared/bo/SelectionItem.java
+++ SchwabenLove/src/de/hdm/grouptwo/shared/bo/SelectionItem.java
@@ -3,18 +3,29 @@ package de.hdm.grouptwo.shared.bo;
 public class SelectionItem extends BusinessObject {
 
 	/**
+	 * Name is the text of a selectionItem, 
 	 * SelectionItem is a Item which is predefined by the system
+	 * 
 	 * @author DenisThierry 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private String selectionText;
+	private String name;
+	private int property_id;
 
-	public String getSelectionText() {
-		return selectionText;
+	public String getName() {
+		return name;
 	}
 
-	public void setSelectionText(String selectionText) {
-		this.selectionText = selectionText;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getProperty_id() {
+		return property_id;
+	}
+
+	public void setProperty_id(int property_id) {
+		this.property_id = property_id;
 	}
 }
