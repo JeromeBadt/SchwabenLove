90056d35d84a43f5457ab2aed702dd928d0701d9
JeromeBadt <jb163@hdm-stuttgart.de>
2016-05-04 04:48:35 +0200

    Update Profile business object and associated mapper class

--- SchwabenLove/src/de/hdm/grouptwo/shared/bo/Profile.java
+++ SchwabenLove/src/de/hdm/grouptwo/shared/bo/Profile.java
@@ -1,125 +1,126 @@
 package de.hdm.grouptwo.shared.bo;
 
 import java.util.Date;
-	
-	
 
+public class Profile extends BusinessObject {
 
-	public class Profile extends BusinessObject{
+	private static final long serialVersionUID = 1L;
 
-	
-		private static final long serialVersionUID = 1L;
-		
-		private String firstName;
-		private String lastName;
-		private String gender;
-		private Date birthdate;
-		private String location;
-		private String hairColor;
-		private String pysique;
-		private String smoker;
-		private int height;
-		private String religion;
-		private String education;
-		private String email;
-		
-		public void setFirstName (String firstName){
-			this.firstName = firstName;
-		}
-		
-		public String getFirstName(){
-			return firstName;
-		}
-		
-		public void setLastName (String lastName){
-			this.lastName=lastName;
-		}
-		
-		public String getLastName(){
-			return lastName;
-		}
-		
-		public String getGender(){
-			return gender;
-		}
-		
-		public void setGender(String gender){
-			this.gender=gender;
-		}
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private Date birthdate;
+	private String location;
+	private int height;
+	private String pysique;
+	private String hairColor;
+	private String smoker;
+	private String education;
+	private String profession;
+	private String religion;
 
-		public Date getBirthdate() {
-			return birthdate;
-		}
-
-		public void setBirthdate(Date birthdate) {
-			this.birthdate = birthdate;
-		}
-
-		public String getLocation() {
-			return location;
-		}
-
-		public void setLocation(String location) {
-			this.location = location;
-		}
-
-		public String getHairColor() {
-			return hairColor;
-		}
-
-		public void setHairColor(String hairColor) {
-			this.hairColor = hairColor;
-		}
-
-		public String getPysique() {
-			return pysique;
-		}
-
-		public void setPysique(String pysique) {
-			this.pysique = pysique;
-		}
-
-		public String getSmoker() {
-			return smoker;
-		}
-
-		public void setSmoker(String smoker) {
-			this.smoker = smoker;
-		}
-
-		public int getHeight() {
-			return height;
-		}
-
-		public void setHeight(int height) {
-			this.height = height;
-		}
-
-		public String getReligion() {
-			return religion;
-		}
-
-		public void setReligion(String religion) {
-			this.religion = religion;
-		}
-
-		public String getEducation() {
-			return education;
-		}
-
-		public void setEducation(String education) {
-			this.education = education;
-		}
-
-		public String getEmail() {
-			return email;
-		}
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		
-		
+	public String getEmail() {
+		return email;
 	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getHairColor() {
+		return hairColor;
+	}
+
+	public void setHairColor(String hairColor) {
+		this.hairColor = hairColor;
+	}
+
+	public String getPysique() {
+		return pysique;
+	}
+
+	public void setPysique(String pysique) {
+		this.pysique = pysique;
+	}
+
+	public String getSmoker() {
+		return smoker;
+	}
+
+	public void setSmoker(String smoker) {
+		this.smoker = smoker;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public String getEducation() {
+		return education;
+	}
+
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+	
+	public String getReligion() {
+		return religion;
+	}
+
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+}
