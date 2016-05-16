8c165a2524ce3d735155156288093d9f9ffae0d1
JoshuaHill <jh147@hdm-stuttgart.de>
2016-05-16 19:31:03 +0200

    Add initial structure for the GUI layout

--- SchwabenLove/src/de/hdm/grouptwo/client/SchwabenLove.java
+++ SchwabenLove/src/de/hdm/grouptwo/client/SchwabenLove.java
@@ -1,6 +1,7 @@
 package de.hdm.grouptwo.client;
 
 import de.hdm.grouptwo.shared.FieldVerifier;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -12,6 +13,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -24,6 +26,9 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class SchwabenLove implements EntryPoint {
 	
 	public void onModuleLoad() {
+		
+		MainView mainView = new MainView();
+		RootPanel.get().add(mainView);
 		
 	}
 //	/**
