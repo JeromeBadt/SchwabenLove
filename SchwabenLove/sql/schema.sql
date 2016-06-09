c068e3701e6d53e0f2eeedc8a4fb577ca0c3b090
JoshuaHill <jh147@hdm-stuttgart.de>
2016-06-09 15:24:53 +0200

    modify sql

--- SchwabenLove/sql/schema.sql
+++ SchwabenLove/sql/schema.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE SCHEMA IF NOT EXISTS `schwabenlove` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `schwabenlove` ;
+
 --
 -- Table structure for table `block`
 --
