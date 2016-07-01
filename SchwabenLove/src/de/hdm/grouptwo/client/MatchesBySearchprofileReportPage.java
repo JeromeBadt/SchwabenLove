4c30a22beb5f2da4e9a1f5e48a0a1ec1f970007f
JoshuaHill <jh147@hdm-stuttgart.de>
2016-07-01 11:39:41 +0200

    implement ReportGenerator Menu

---
+++ SchwabenLove/src/de/hdm/grouptwo/client/MatchesBySearchprofileReportPage.java
@@ -0,0 +1,18 @@
+package de.hdm.grouptwo.client;
+
+public class MatchesBySearchprofileReportPage extends ContentPage {
+
+	public MatchesBySearchprofileReportPage() {
+		super("Partnervorschläge anhand von Suchprofil anzeigen");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updatePage() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
---
+++ SchwabenLove/src/de/hdm/grouptwo/client/UnviewedMatchesReportPage.java
@@ -0,0 +1,16 @@
+package de.hdm.grouptwo.client;
+
+public class UnviewedMatchesReportPage extends ContentPage{
+
+	public UnviewedMatchesReportPage() {
+		super("Nicht gesehene Partnervorschläge anzeigen");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updatePage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
