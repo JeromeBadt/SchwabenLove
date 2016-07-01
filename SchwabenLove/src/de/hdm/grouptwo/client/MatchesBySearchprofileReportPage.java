eac9c6f4598d8dc4161db06a6119c53fe6cb8ca0
JoshuaHill <jh147@hdm-stuttgart.de>
2016-07-01 16:47:07 +0200

    Add init panel to RepGenGui

--- SchwabenLove/src/de/hdm/grouptwo/client/MatchesBySearchprofileReportPage.java
+++ SchwabenLove/src/de/hdm/grouptwo/client/MatchesBySearchprofileReportPage.java
@@ -1,14 +1,22 @@
 package de.hdm.grouptwo.client;
 
+import com.google.gwt.user.client.ui.LayoutPanel;
+
 import de.hdm.grouptwo.shared.ReportServiceAsync;
 
 public class MatchesBySearchprofileReportPage extends ContentPage {
+	private LayoutPanel lPanel = new LayoutPanel();
+	private LayoutPanel matchesPanel = new LayoutPanel();
 
 	private ReportServiceAsync reportService = ClientsideSettings.getReportService();
 	
 	public MatchesBySearchprofileReportPage() {
 		super("Partnervorschläge anhand von Suchprofil anzeigen");
-		// TODO Auto-generated constructor stub
+		initWidget(lPanel);
+		lPanel.setStyleName("matches-page");
+		matchesPanel.setStyleName("matches");
+		
+		lPanel.add(matchesPanel);
 	}
 
 	@Override
--- SchwabenLove/src/de/hdm/grouptwo/client/UnviewedMatchesReportPage.java
+++ SchwabenLove/src/de/hdm/grouptwo/client/UnviewedMatchesReportPage.java
@@ -1,14 +1,22 @@
 package de.hdm.grouptwo.client;
 
+import com.google.gwt.user.client.ui.LayoutPanel;
+
 import de.hdm.grouptwo.shared.ReportServiceAsync;
 
 public class UnviewedMatchesReportPage extends ContentPage{
+	private LayoutPanel lPanel = new LayoutPanel();
+	private LayoutPanel matchesPanel = new LayoutPanel();
 	
 	private ReportServiceAsync reportService = ClientsideSettings.getReportService();
 
 	public UnviewedMatchesReportPage() {
 		super("Nicht gesehene Partnervorschläge anzeigen");
-		// TODO Serverseitige Methode aufrufen
+		initWidget(lPanel);
+		lPanel.setStyleName("matches-page");
+		matchesPanel.setStyleName("matches");
+		
+		lPanel.add(matchesPanel);
 	}
 
 	@Override
